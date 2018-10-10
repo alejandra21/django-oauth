00eb2e491ef94a28724e03fe24d02c8c8d5b3198
Alejandra Cordero <acorderog@socialh4ck.com>
2018-10-10 18:31:18 -0400

    -Se agrego un poco de estilo

--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from .views import home
+from .views import home, logout_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^logout/$', logout_view, name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
     url(r'^$', home, name='home'),
 ]
--- testapp/testapp/views.py
+++ testapp/testapp/views.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 @login_required
 def home(request):
 	print(dir(request.user))
 	return render(request, 'testapp/home.html')
+
+
+def logout_view(request):
+	logout(request)
+	return redirect('/')
